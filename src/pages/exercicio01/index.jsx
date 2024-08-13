3c754d0acf67b7351bf9cc94bb05003e5dd21491
ryankkzy <ryan37370308@gmail.com>
2024-08-13 17:15:05 -0300

    exercicio 1 feito

--- src/pages/exercicio01/index.jsx
+++ src/pages/exercicio01/index.jsx
@@ -1,6 +1,17 @@
 import './index.scss'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
 export default function Exerci1() {
+  const [valorPedido, setValorPedido] = useState(0);
+  const [valorCupom, setValorCupom] = useState(0);
+  const [valorTotal , setValorTotal] = useState(0)
+
+
+  function calcularValorTotal() {
+    let total = valorPedido - valorCupom
+    setValorTotal(total)
+  }
 
     return(
     <div className='pagina-exercicio1 pagina'>
@@ -20,6 +31,28 @@ export default function Exerci1() {
           </div>
         </div>
       </header>
+      <div className='informacao'>
+        <img src="/assets/images/voltar.png" alt="" />
+        <h2>Exercicio 01 - Cupom de desconto</h2>
+      </div>
+      <div className='faixa' />
+      <div className='caixa-problema'>
+        <p>Implementar um programa em Javascript para calcular o valor final de uma compra a partir do valor da compra e do cupom de desconto. O cupom diz quantos reais terá <br /> de desconto.</p>
+      </div>
+      <div className='campos'>
+        <div className='problema'>
+          <label>Informe o valor do pedido</label>
+          <input placeholder='0' onChange={e => setValorPedido(e.target.value)} />
+        </div>
+        <div className='problema'>
+          <label>Informe o valor do cupom</label>
+          <input placeholder='0' onChange={e => setValorCupom(e.target.value)} />
+        </div>
+      <div>
+        <button href='#' className='executar' onClick={calcularValorTotal}>Executar</button>
+      </div>        
+      </div>
+      <h3 className='resultado'>Resultado: O total é R$ {valorTotal.toFixed(2)}</h3>
     </div>
     )
 }
